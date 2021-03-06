2edaef1208c8d6bd482b57d13635946bf0c82291
Naymov Aleksey <n.a.v.ru@mail.ru>
2021-03-06 04:22:08 +0300

    styling the promo

--- src/components/promo/promo.jsx
+++ src/components/promo/promo.jsx
@@ -4,9 +4,12 @@ const Promo = () => {
   return (
     <section className="promo">
       <div className="promo__wrapper">
-        <h2 className="promo__title">Лига Банк</h2>
-        <p className="promo__text">Кредиты на любой случай</p>
-        <a className="promo__link button button--light">Рассчитать кредит</a>
+        <div className="promo__container">
+          <h2 className="promo__title">Лига Банк</h2>
+          <p className="promo__text">Кредиты на любой случай</p>
+          <a className="promo__link button button--light">Рассчитать кредит</a>
+        </div>
+
       </div>
     </section>
   );
